666b6d377649d965a39c5755bd99173a67c9f8fe
Ajith Pattammattel <ajith.pattammattel@gmail.com>
2021-03-15 17:40:43 -0400

    2.0

--- Scan/hxn_gui_2.0.py
+++ Scan/hxn_gui_2.0.py
@@ -3,13 +3,20 @@
 #Author: Ajith Pattammattel
 #Original Date:06-23-2020
 
-import sys, os, signal, subprocess
-import numpy as np
+import os
+import signal
+import subprocess
+import sys
+import collections
+import pyqtgraph as pg
+from scipy.ndimage import rotate
+
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
 
 from pdf_log import *
-from xanes2d import*
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from xanes2d import *
+
 
 
 class Ui(QtWidgets.QMainWindow):
@@ -17,6 +24,7 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         uic.loadUi('/home/xf03id/user_macros/HXN_GUI/Scan/hxn_gui_admin2.ui', self)
         self.initParams()
+        self.ImageCorrelationPage()
 
         # updating resolution/tot time
         self.dwell.valueChanged.connect(self.initParams)
@@ -46,6 +54,7 @@ class Ui(QtWidgets.QMainWindow):
         self.pb_close_all_plot.clicked.connect(self.close_all_plots)
         self.pb_close_plot.clicked.connect(self.close_plot)
         self.pb_plot.clicked.connect(self.plot_me)
+        self.pb_erf_fit.clicked.connect(self.plot_erf_fit)
 
         #generate xanes parameters
         self.pb_gen_elist.clicked.connect(self.generate_elist)
@@ -57,6 +66,7 @@ class Ui(QtWidgets.QMainWindow):
         self.pb_move_smary.clicked.connect(self.move_smary)
         self.pb_move_smarz.clicked.connect(self.move_smarz)
         self.pb_move_dth.clicked.connect(self.move_dsth)
+        self.pb_move_zpz.clicked.connect(self.move_zpz1)
         
         #Detector/Camera Motions
         self.pb_merlinOUT.clicked.connect(self.merlinOUT)
@@ -66,7 +76,6 @@ class Ui(QtWidgets.QMainWindow):
         self.pb_cam6IN.clicked.connect(self.cam6IN)
         self.pb_cam6OUT.clicked.connect(self.cam6OUT)
         self.pb_cam11IN.clicked.connect(self.cam11IN)
-        
 
         #Quick fill scan Params
         self.pb_3030.clicked.connect(self.fill_common_scan_params)
@@ -77,16 +86,15 @@ class Ui(QtWidgets.QMainWindow):
         #admin control
         self.pb_apply_user_settings.clicked.connect(self.setUserLevel)
 
-        '''
         #elog 
         self.pb_folder_log.clicked.connect(self.select_pdf_wd)
         self.pb_pdf_image.clicked.connect(self.select_pdf_image)
         self.pb_date_ok.clicked.connect(self.generate_pdf)
         self.pb_save_pdf.clicked.connect(self.force_save_pdf)
         self.pb_createpdf.clicked.connect(self.insert_pdf)
-        '''
+
         #close the application
-        #self.pb_exit.clicked.connect(self.close_application)
+        self.actionClose_Application.triggered.connect(self.close_application)
 
         self.show()
 
@@ -95,7 +103,6 @@ class Ui(QtWidgets.QMainWindow):
         self.userButtonEnabler(self.cb_det_user, self.gb_det_control)
         self.userButtonEnabler(self.cb_xanes_user, self.rb_xanes_scan)
         self.userButtonEnabler(self.cb_xanes_user, self.gb_xanes_align)
-
 
     def userButtonEnabler(self,checkbox_name,control_btn_grp_name):
 
@@ -117,7 +124,6 @@ class Ui(QtWidgets.QMainWindow):
 
         self.dwell_t = self.dwell.value()
 
-
     def initParams(self):
         self.getScanValues()
 
@@ -130,7 +136,7 @@ class Ui(QtWidgets.QMainWindow):
         if self.rb_1d.isChecked():
 
             self.label_scanMacro.setText(f'fly1d({self.det}, {self.mot1_s}, '
-                                         f'{self.mot1_e}, {self.mot1_steps}, {self.dwell_t})')
+                                         f'{self.mot1_e}, {self.mot1_steps}, {self.dwell_t:.3f})')
 
         else:
             self.label_scanMacro.setText(f'fly2d({self.det}, {self.mot1_s}, {self.mot1_e}, {self.mot1_steps}, '
@@ -154,7 +160,6 @@ class Ui(QtWidgets.QMainWindow):
         else:
             RE(fly2d(self.det_list[self.det], self.motor_list[self.motor1], self.mot1_s,self.mot1_e ,self.mot1_steps,
                      self.motor_list[self.motor2], self.mot2_s,self.mot2_e, self.mot2_steps, self.dwell_t))
-
 
     def disableMot2(self):
         self.y_start.setEnabled(False)
@@ -199,12 +204,15 @@ class Ui(QtWidgets.QMainWindow):
 
     def move_dsth(self):
         self.moveAMotor(self.db_move_dth, zpsth)
+
+    def move_zpz1(self):
+        RE(movr_zpz1(self.db_move_zpz.value()*0.001))
         
     def merlinIN(self):
         RE(go_det('merlin'))
         
     def merlinOUT(self):
-        RE(bps.mov(diff.x,-500))
+        RE(bps.mov(diff.x,-600))
                 
     def vortexIN(self):
         RE(bps.mov(fdet1.x,-8))
@@ -254,10 +262,12 @@ class Ui(QtWidgets.QMainWindow):
 
         sd = self.lineEdit_5.text()
         elem = self.lineEdit_6.text()
-        if self.ch_b_norm.isChecked():
-            plot_data(int(sd), elem, 'sclr1_ch4')
-        else:
-            plot_data(sd, elem)
+        plot_data(int(sd), elem, 'sclr1_ch4')
+
+    def plot_erf_fit(self):
+        sd = self.lineEdit_5.text()
+        elem = self.lineEdit_6.text()
+        erf_fit(int(sd),elem, linear_flag = self.cb_erf_linear_flag.isChecked())
             
     def save_file(self):
       S__File = QFileDialog.getSaveFileName(None,'SaveFile','/', "Python Files (*.py)")
@@ -283,7 +293,6 @@ class Ui(QtWidgets.QMainWindow):
         file_name = QFileDialog().getOpenFileName(self, "Open file")
         self.le_ex_macro.setText(str(file_name[0]))
 
-
     def open_a_macro(self):
         editor = self.cb_ex_macro_with.currentText()
         filename = self.le_ex_macro.text()
@@ -307,8 +316,7 @@ class Ui(QtWidgets.QMainWindow):
     def select_pdf_image(self):
         file_name = QFileDialog().getOpenFileName(self, "Select an Image")
         self.le_elog_image.setText(str(file_name[0]))
-        
-    
+
     def generate_pdf(self):
         dt = self.dateEdit_elog.date()
         tmp_date = dt.toString(self.dateEdit_elog.displayFormat())
@@ -325,6 +333,196 @@ class Ui(QtWidgets.QMainWindow):
     def force_save_pdf(self):
         return save_page_for_gui()
 
+    def ImageCorrelationPage(self):
+
+        self.coords = collections.deque(maxlen=4)
+
+        #connections
+        self.pb_RefImageLoad.clicked.connect(self.loadRefImage)
+        self.pb_apply_calculation.clicked.connect(self.scalingCalculation)
+        self.dsb_x_off.valueChanged.connect(self.offsetCorrectedPos)
+        self.dsb_y_off.valueChanged.connect(self.offsetCorrectedPos)
+        self.pb_grabXY_1.clicked.connect(self.insertCurrentPos1)
+        self.pb_grabXY_2.clicked.connect(self.insertCurrentPos2)
+        #self.pb_grabXY_2.clicked.connect(self.insertCurrentPos(self.dsb_ref2_x,self.dsb_ref2_y))
+        self.pb_gotoTargetPos.clicked.connect(self.gotoTargetPos)
+
+    def loadRefImage(self):
+        self.file_name = QtWidgets.QFileDialog().getOpenFileName(self, "Select Ref Image", '',
+                                                                 'image file(*png *jpeg *tiff *tif )')
+        if self.file_name:
+            self.ref_image = plt.imread(self.file_name[0])
+            if self.ref_image.ndim == 3:
+                self.ref_image = self.ref_image.sum(2)
+            self.statusbar.showMessage(f'{self.file_name[0]} selected')
+        else:
+            self.statusbar.showMessage("No file has selected")
+            pass
+
+        try:
+            self.ref_view.clear()
+        except:
+            pass
+
+
+        # A plot area (ViewBox + axes) for displaying the image
+        self.p1 = self.ref_view.addPlot(title="")
+
+        # Item for displaying image data
+        self.img = pg.ImageItem()
+        hist = pg.HistogramLUTItem()
+        hist.setImageItem(self.img)
+        self.ref_view.addItem(hist)
+
+        self.p1.addItem(self.img)
+        self.ref_image = rotate(self.ref_image, -90)
+        self.img.setImage(self.ref_image)
+        self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
+        # self.img.translate(100, 50)
+        # self.img.scale(0.5, 0.5)
+        self.img.hoverEvent = self.imageHoverEvent
+        self.img.mousePressEvent = self.MouseClickEvent
+
+    def imageHoverEvent(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.isExit():
+            self.p1.setTitle("")
+            return
+        pos = event.pos()
+        i, j = pos.x(), pos.y()
+        i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+        j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+        val = self.ref_image[i, j]
+        ppos = self.img.mapToParent(pos)
+        x, y = np.around(ppos.x(), 2), np.around(ppos.y(), 2)
+        self.p1.setTitle(f'pos: {x, y}  pixel: {i, j}  value: {val}')
+
+    def MouseClickEvent(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.button() == QtCore.Qt.LeftButton:
+
+            pos = event.pos()
+            i, j = pos.x(), pos.y()
+            i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+            j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+            self.coords.append((i, j))
+            val = self.ref_image[i, j]
+            ppos = self.img.mapToParent(pos)
+            #x, y = np.around(ppos.x(), 2) , np.around(ppos.y(), 2)
+            x, y = smarx.position, smary.position
+            self.coords.append((x, y))
+            if len(self.coords) == 2:
+                self.le_ref1_pxls.setText(f'{self.coords[0][0]}, {self.coords[0][1]}')
+                self.dsb_ref1_x.setValue(self.coords[1][0])
+                self.dsb_ref1_y.setValue(self.coords[1][1])
+            elif len(self.coords) == 4:
+                self.le_ref1_pxls.setText(f'{self.coords[0][0]}, {self.coords[0][1]}')
+                self.dsb_ref1_x.setValue(self.coords[1][0])
+                self.dsb_ref1_y.setValue(self.coords[1][1])
+                self.le_ref2_pxls.setText(f'{self.coords[2][0]}, {self.coords[2][1]}')
+                self.dsb_ref2_x.setValue(self.coords[-1][0])
+                self.dsb_ref2_y.setValue(self.coords[-1][1])
+
+    def createLabAxisImage(self):
+        # A plot area (ViewBox + axes) for displaying the image
+
+        try:
+            self.labaxis_view.clear()
+        except:
+            pass
+
+
+        self.p2 = self.labaxis_view.addPlot(title="")
+
+        # Item for displaying image data
+        self.img2 = pg.ImageItem()
+        hist = pg.HistogramLUTItem()
+        hist.setImageItem(self.img2)
+        self.labaxis_view.addItem(hist)
+        self.p2.addItem(self.img2)
+        self.img2.setImage(self.ref_image)
+        self.img2.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
+        #self.img2.setImage(self.ref_image.T,opacity = 0.5)
+
+    def scalingCalculation(self):
+        yshape, xshape = np.shape(self.ref_image)
+
+        lm1_px, lm1_py = self.le_ref1_pxls.text().split(',')  # r chooses this pixel
+        lm2_px, lm2_py = self.le_ref2_pxls.text().split(',')  # chooses this pixel
+
+        lm1_x, lm1_y = self.dsb_ref1_x.value(), self.dsb_ref1_y.value()  # motor values from the microscope at pixel pos 1
+        lm2_x, lm2_y = self.dsb_ref2_x.value(), self.dsb_ref2_y.value()  # motor values from the microscope at pixel pos 2
+
+        self.pixel_val_x = (lm2_x - lm1_x) / (int(lm2_px) - int(lm1_px))  # pixel value of X
+        self.pixel_val_y = (lm2_y - lm1_y) / (int(lm2_py) - int(lm1_py))  # pixel value of Y; ususally same as X
+
+        self.xi = lm1_x - (self.pixel_val_x * int(lm1_px))  # xmotor pos at origin (0,0)
+        xf = self.xi + (self.pixel_val_x * xshape)  # xmotor pos at the end (0,0)
+        self.yi = lm1_y - (self.pixel_val_y * int(lm1_py))  # xmotor pos at origin (0,0)
+        yf = self.yi + (self.pixel_val_y * yshape)  # xmotor pos at origin (0,0)
+        self.createLabAxisImage()
+
+        self.label_scale_info.setText(f'Scaling: {self.pixel_val_x:.4f}, {self.pixel_val_y:.4f}, \n '
+                                      f' X Range {self.xi:.2f}:{xf:.2f}, \n'
+                                      f'Y Range {self.yi:.2f}:{yf:.2f}')
+        self.img2.scale(abs(self.pixel_val_x), abs(self.pixel_val_y))
+        self.img2.translate(self.xi, self.yi)
+        #self.img2.setRect(QtCore.QRect(xi,yf,yi,xf))
+        self.img2.hoverEvent = self.imageHoverEvent2
+        self.img2.mousePressEvent = self.MouseClickEventToPos
+
+    def imageHoverEvent2(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.isExit():
+            self.p2.setTitle("")
+            return
+        pos = event.pos()
+        i, j = pos.x(), pos.y()
+        i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+        j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+        val = self.ref_image[i, j]
+        x = self.xi + (self.pixel_val_x * i)
+        y = self.yi + (self.pixel_val_y * j)
+        self.p2.setTitle(f'pos: {x:.2f},{y:.2f}  pixel: {i, j}  value: {val:.2f}')
+
+    def MouseClickEventToPos(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.button() == QtCore.Qt.LeftButton:
+            pos = event.pos()
+            i, j = pos.x(), pos.y()
+            i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+            j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+            self.xWhere = self.xi + (self.pixel_val_x * i)
+            self.yWhere = self.yi + (self.pixel_val_y * j)
+            self.offsetCorrectedPos()
+
+    def offsetCorrectedPos(self):
+        self.dsb_calc_x.setValue(self.xWhere + (self.dsb_x_off.value() * 0.001))
+        self.dsb_calc_y.setValue(self.yWhere + (self.dsb_y_off.value() * 0.001))
+
+    def insertCurrentPos1(self):
+        posX = smarx.position
+        posY = smary.position
+
+        self.dsb_ref1_x.setValue(posX)
+        self.dsb_ref1_y.setValue(posY)
+
+    def insertCurrentPos2(self):
+        posX = smarx.position
+        posY = smary.position
+
+        self.dsb_ref2_x.setValue(posX)
+        self.dsb_ref2_y.setValue(posY)
+
+    def gotoTargetPos(self):
+        targetX = self.dsb_calc_x.value()
+        targetY = self.dsb_calc_y.value()
+        RE(bps.mov(smarx, targetX))
+        RE(bps.mov(smary, targetY))
 
     def close_application(self):
 
---
+++ Scan/imageCorrelation.py
@@ -0,0 +1,214 @@
+import sys
+import os
+import collections
+import tifffile as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import pyqtgraph as pg
+from scipy.ndimage import rotate
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets, uic
+
+
+class ImageCorrelationWindow(QtWidgets.QMainWindow):
+    def __init__(self, ref_image=None):
+        super(ImageCorrelationWindow, self).__init__()
+        uic.loadUi('/home/xf03id/user_macros/HXN_GUI/Scan/imageCorrelation.ui', self)
+        self.ref_image = ref_image
+        self.coords = collections.deque(maxlen=4)
+
+        # connections
+        self.actionLoad_refImage.triggered.connect(self.loadRefImage)
+        self.pb_apply_calculation.clicked.connect(self.scalingCalculation)
+        self.dsb_x_off.valueChanged.connect(self.offsetCorrectedPos)
+        self.dsb_y_off.valueChanged.connect(self.offsetCorrectedPos)
+        self.pb_grabXY_1.clicked.connect(self.insertCurrentPos1)
+        self.pb_grabXY_2.clicked.connect(self.insertCurrentPos2)
+        #self.pb_grabXY_2.clicked.connect(self.insertCurrentPos(self.dsb_ref2_x,self.dsb_ref2_y))
+        self.pb_gotoTargetPos.clicked.connect(self.gotoTargetPos)
+
+    def loadRefImage(self):
+        self.file_name = QtWidgets.QFileDialog().getOpenFileName(self, "Select Ref Image", '',
+                                                                 'image file(*png *jpeg *tiff *tif )')
+        if self.file_name:
+            self.ref_image = plt.imread(self.file_name[0])
+            if self.ref_image.ndim == 3:
+                self.ref_image = self.ref_image.sum(2)
+            self.statusbar.showMessage(f'{self.file_name[0]} selected')
+        else:
+            self.statusbar.showMessage("No file has selected")
+            pass
+
+        try:
+            self.ref_view.clear()
+        except:
+            pass
+
+
+        # A plot area (ViewBox + axes) for displaying the image
+        self.p1 = self.ref_view.addPlot(title="")
+
+        # Item for displaying image data
+        self.img = pg.ImageItem()
+        hist = pg.HistogramLUTItem()
+        hist.setImageItem(self.img)
+        self.ref_view.addItem(hist)
+
+        self.p1.addItem(self.img)
+        self.ref_image = rotate(self.ref_image, -90)
+        self.img.setImage(self.ref_image)
+        self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
+        # self.img.translate(100, 50)
+        # self.img.scale(0.5, 0.5)
+        self.img.hoverEvent = self.imageHoverEvent
+        self.img.mousePressEvent = self.MouseClickEvent
+
+    def imageHoverEvent(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.isExit():
+            self.p1.setTitle("")
+            return
+        pos = event.pos()
+        i, j = pos.x(), pos.y()
+        i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+        j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+        val = self.ref_image[i, j]
+        ppos = self.img.mapToParent(pos)
+        x, y = np.around(ppos.x(), 2), np.around(ppos.y(), 2)
+        self.p1.setTitle(f'pos: {x, y}  pixel: {i, j}  value: {val}')
+
+    def MouseClickEvent(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.button() == QtCore.Qt.LeftButton:
+
+            pos = event.pos()
+            i, j = pos.x(), pos.y()
+            i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+            j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+            self.coords.append((i, j))
+            val = self.ref_image[i, j]
+            ppos = self.img.mapToParent(pos)
+            x, y = np.around(ppos.x(), 2) , np.around(ppos.y(), 2)
+            # x, y = smarx.pos, smary.pos
+            self.coords.append((x, y))
+            if len(self.coords) == 2:
+                self.le_ref1_pxls.setText(f'{self.coords[0][0]}, {self.coords[0][1]}')
+                self.dsb_ref1_x.setValue(self.coords[1][0])
+                self.dsb_ref1_y.setValue(self.coords[1][1])
+            elif len(self.coords) == 4:
+                self.le_ref1_pxls.setText(f'{self.coords[0][0]}, {self.coords[0][1]}')
+                self.dsb_ref1_x.setValue(self.coords[1][0])
+                self.dsb_ref1_y.setValue(self.coords[1][1])
+                self.le_ref2_pxls.setText(f'{self.coords[2][0]}, {self.coords[2][1]}')
+                self.dsb_ref2_x.setValue(self.coords[-1][0])
+                self.dsb_ref2_y.setValue(self.coords[-1][1])
+
+    def createLabAxisImage(self):
+        # A plot area (ViewBox + axes) for displaying the image
+
+        try:
+            self.labaxis_view.clear()
+        except:
+            pass
+
+
+        self.p2 = self.labaxis_view.addPlot(title="")
+
+        # Item for displaying image data
+        self.img2 = pg.ImageItem()
+        hist = pg.HistogramLUTItem()
+        hist.setImageItem(self.img2)
+        self.labaxis_view.addItem(hist)
+        self.p2.addItem(self.img2)
+        self.img2.setImage(self.ref_image)
+        self.img2.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
+        #self.img2.setImage(self.ref_image.T,opacity = 0.5)
+
+
+    def scalingCalculation(self):
+        yshape, xshape = np.shape(self.ref_image)
+
+        lm1_px, lm1_py = self.le_ref1_pxls.text().split(',')  # r chooses this pixel
+        lm2_px, lm2_py = self.le_ref2_pxls.text().split(',')  # chooses this pixel
+
+        lm1_x, lm1_y = self.dsb_ref1_x.value(), self.dsb_ref1_y.value()  # motor values from the microscope at pixel pos 1
+        lm2_x, lm2_y = self.dsb_ref2_x.value(), self.dsb_ref2_y.value()  # motor values from the microscope at pixel pos 2
+
+        self.pixel_val_x = (lm2_x - lm1_x) / (int(lm2_px) - int(lm1_px))  # pixel value of X
+        self.pixel_val_y = (lm2_y - lm1_y) / (int(lm2_py) - int(lm1_py))  # pixel value of Y; ususally same as X
+
+        self.xi = lm1_x - (self.pixel_val_x * int(lm1_px))  # xmotor pos at origin (0,0)
+        xf = self.xi + (self.pixel_val_x * xshape)  # xmotor pos at the end (0,0)
+        self.yi = lm1_y - (self.pixel_val_y * int(lm1_py))  # xmotor pos at origin (0,0)
+        yf = self.yi + (self.pixel_val_y * yshape)  # xmotor pos at origin (0,0)
+        self.createLabAxisImage()
+
+        self.label_scale_info.setText(f'Scaling: {self.pixel_val_x:.2f}, {self.pixel_val_y:.2f}, \n '
+                                      f' X Range {self.xi:.2f}:{xf:.2f}, \n'
+                                      f'Y Range {self.yi:.2f}:{yf:.2f}')
+        self.img2.scale(abs(self.pixel_val_x), abs(self.pixel_val_y))
+        self.img2.translate(self.xi, self.yi)
+        #self.img2.setRect(QtCore.QRect(xi,yf,yi,xf))
+        self.img2.hoverEvent = self.imageHoverEvent2
+        self.img2.mousePressEvent = self.MouseClickEventToPos
+
+    def imageHoverEvent2(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.isExit():
+            self.p2.setTitle("")
+            return
+        pos = event.pos()
+        i, j = pos.x(), pos.y()
+        i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+        j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+        val = self.ref_image[i, j]
+        x = self.xi + (self.pixel_val_x * i)
+        y = self.yi + (self.pixel_val_y * j)
+        self.p2.setTitle(f'pos: {x:.2f},{y:.2f}  pixel: {i, j}  value: {val:.2f}')
+
+    def MouseClickEventToPos(self, event):
+        """Show the position, pixel, and value under the mouse cursor.
+        """
+        if event.button() == QtCore.Qt.LeftButton:
+            pos = event.pos()
+            i, j = pos.x(), pos.y()
+            i = int(np.clip(i, 0, self.ref_image.shape[0] - 1))
+            j = int(np.clip(j, 0, self.ref_image.shape[1] - 1))
+            self.xWhere = self.xi + (self.pixel_val_x * i)
+            self.yWhere = self.yi + (self.pixel_val_y * j)
+            self.offsetCorrectedPos()
+
+    def offsetCorrectedPos(self):
+        self.dsb_calc_x.setValue(self.xWhere + (self.dsb_x_off.value() * 0.001))
+        self.dsb_calc_y.setValue(self.yWhere + (self.dsb_y_off.value() * 0.001))
+
+    def insertCurrentPos1(self):
+        posX = smarx.position
+        posY = smary.position
+
+        self.dsb_ref1_x.setValue(posX)
+        self.dsb_ref1_y.setValue(posY)
+
+    def insertCurrentPos2(self):
+        posX = smarx.position
+        posY = smary.position
+
+        self.dsb_ref2_x.setValue(posX)
+        self.dsb_ref2_y.setValue(posY)
+
+    def gotoTargetPos(self):
+        targetX = self.dsb_calc_x.value()
+        targetY = self.dsb_calc_y.value()
+        RE(bps.mov(smarx, targetX))
+        RE(bps.mov(smary, targetY))
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    # app.setAttribute(QtCore.Qt.AA_Use96Dpi)
+    window = ImageCorrelationWindow()
+    window.show()
+    sys.exit(app.exec_())
