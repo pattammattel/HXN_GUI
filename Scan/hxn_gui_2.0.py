30847d7e956d9ec1141ff8a28433afbb6f05d30e
Ajith Pattammattel <ajith.pattammattel@gmail.com>
2021-04-13 16:00:31 -0400

    intial commit

--- Scan/hxn_gui_2.0.py
+++ Scan/hxn_gui_2.0.py
@@ -906,51 +906,6 @@ class Ui(QtWidgets.QMainWindow):
         else:
             pass
 
-class WorkerSignals(QObject):
-    '''
-    Defines the signals available from a running worker thread.
-    Supported signals are:
-    - finished: No data
-    - error:`tuple` (exctype, value, traceback.format_exc() )
-    - result: `object` data returned from processing, anything
-    - progress: `tuple` indicating progress metadata
-    '''
-    finished = pyqtSignal()
-    error = pyqtSignal(tuple)
-    result = pyqtSignal(object)
-
-class Worker(QRunnable):
-    '''
-    Worker thread
-    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
-    '''
-
-    def __init__(self, fn, *args, **kwargs):
-        super(Worker, self).__init__()
-        # Store constructor arguments (re-used for processing)
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.signals = WorkerSignals()
-
-    @pyqtSlot()
-    def run(self):
-        '''
-        Initialise the runner function with passed args, kwargs.
-        '''
-        # Retrieve args/kwargs here; and fire processing using them
-        try:
-            result = self.fn(*self.args, **self.kwargs)
-        except:
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            self.signals.error.emit((exctype, value, traceback.format_exc()))
-        else:
-            self.signals.result.emit(result)  # Return the result of the processing
-        finally:
-            self.signals.finished.emit()  # Done
-
-
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = Ui()
