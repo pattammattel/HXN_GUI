6e982a97bb0a432cd99c17f955a5de343d535a42
Ajith Pattammattel <ajith.pattammattel@gmail.com>
2021-04-16 10:55:28 -0400

    2.3

--- Scan/imageCorrelation.py
+++ Scan/imageCorrelation.py
@@ -127,46 +127,51 @@ class ImageCorrelationWindow(QtWidgets.QMainWindow):
     def loadRefImage(self):
         self.file_name = QtWidgets.QFileDialog().getOpenFileName(self, "Select Ref Image", '',
                                                                  'image file(*png *jpeg *tiff *tif )')
-
-        if self.file_name:
+        if self.file_name[0]:
             self.ref_image = cv2.imread(self.file_name[0])
             self.ref_image = cv2.cvtColor(self.ref_image, cv2.COLOR_BGR2RGB)
             self.yshape, self.xshape = np.shape(self.ref_image)[:2]
+
+        
+            try:
+                self.ref_view.clear()
+            except:
+                pass
+
+            # A plot area (ViewBox + axes) for displaying the image
+            self.p1 = self.ref_view.addPlot(title="")
+            self.p1.getViewBox().invertY(True)
+            # Item for displaying image data
+            self.img = pg.ImageItem()
+            self.p1.addItem(self.img)
+            self.img.setImage(self.ref_image)
+            self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
+            # self.img.translate(100, 50)
+            # self.img.scale(0.5, 0.5)
+
+            self.img.hoverEvent = self.imageHoverEvent
+            self.img.mousePressEvent = self.MouseClickEvent
+
         else:
             self.statusbar.showMessage("No file has selected")
             pass
-        
-        try:
-            self.ref_view.clear()
-        except:
-            pass
-
-        # A plot area (ViewBox + axes) for displaying the image
-        self.p1 = self.ref_view.addPlot(title="")
-        self.p1.getViewBox().invertY(True)
-        # Item for displaying image data
-        self.img = pg.ImageItem()
-        self.p1.addItem(self.img)
-        self.img.setImage(self.ref_image)
-        self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
-        # self.img.translate(100, 50)
-        # self.img.scale(0.5, 0.5)
-        
-        self.img.hoverEvent = self.imageHoverEvent
-        self.img.mousePressEvent = self.MouseClickEvent
 
     def loadSecondRefImage(self):
         file_name = QtWidgets.QFileDialog().getOpenFileName(self, "Select Ref Image", '',
                                                                  'image file(*png *jpeg *tiff *tif )')
-        if file_name:
+        if file_name[0]:
             self.ref_image2 = cv2.imread(file_name[0])
             self.ref_image2 = cv2.cvtColor(self.ref_image2, cv2.COLOR_BGR2RGB)
 
-        self.img3 = pg.ImageItem()
-        self.p1.addItem(self.img3)
-        self.img3.setImage(self.ref_image2,opacity=0.5)
-        self.img3.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
-        self.img.setZValue(10)
+
+            self.img3 = pg.ImageItem()
+            self.p1.addItem(self.img3)
+            self.img3.setImage(self.ref_image2)
+            self.img3.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
+            self.img.setZValue(10)
+
+        else:
+            pass
 
     def imageHoverEvent(self, event):
         """Show the position, pixel, and value under the mouse cursor.
