0ce07e5eba9cade737d2a3f8ed40650fa1653abb
Ajith Pattammattel <ajith.pattammattel@gmail.com>
2021-04-20 14:44:01 -0400

    multichannel view

--- Scan/MuliChannel.py
+++ Scan/MuliChannel.py
@@ -3,7 +3,11 @@ import cv2
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets, uic
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import tifffile as tf
 pg.setConfigOption('imageAxisOrder', 'row-major')
+from pyqtgraph.graphicsItems.GradientEditorItem import Gradients
 
 
 class MultiChannelWindow(QtWidgets.QMainWindow):
@@ -11,32 +15,30 @@ class MultiChannelWindow(QtWidgets.QMainWindow):
         super(MultiChannelWindow, self).__init__()
         uic.loadUi('mutlichannel.ui', self)
 
+        self.actionLoad_1.triggered.connect(self.loadImage)
+        self.actionLoad_2.triggered.connect(self.loadImage2)
+        self.actionLoad_3.triggered.connect(self.loadImage3)
+
     def loadImage(self):
         self.file_name = QtWidgets.QFileDialog().getOpenFileName(self, "Select Ref Image", '',
                                                                  'image file(*png *jpeg *tiff *tif )')
         if self.file_name[0]:
-            self.ref_image = cv2.imread(self.file_name[0])
-            self.ref_image = cv2.cvtColor(self.ref_image, cv2.COLOR_BGR2RGB)
-            self.yshape, self.xshape = np.shape(self.ref_image)[:2]
-
-            try:
-                self.ref_view.clear()
-            except:
-                pass
+            self.ref_image = tf.imread(self.file_name[0])
+            #self.ref_image = cv2.imread(self.file_name[0])
+            #self.ref_image = cv2.cvtColor(self.ref_image, cv2.COLOR_BGR2RGB)
 
             # A plot area (ViewBox + axes) for displaying the image
-            self.p1 = self.ref_view.addPlot(title="")
+            self.p1 = self.img_view.addPlot(title="")
             self.p1.getViewBox().invertY(True)
             # Item for displaying image data
             self.img = pg.ImageItem()
             self.p1.addItem(self.img)
-            self.img.setImage(self.ref_image)
-            self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
-            # self.img.translate(100, 50)
-            # self.img.scale(0.5, 0.5)
+            colormap = cm.get_cmap("Reds")  # cm.get_cmap("CMRmap")
+            colormap._init()
+            lut = (colormap._lut * 255).view(np.ndarray)  # Convert matplotlib colormap from 0-1 to 0 -255 for Qt
+            self.img.setImage(self.ref_image, lut = lut)
+            #self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Multiply)
 
-            self.img.hoverEvent = self.imageHoverEvent
-            self.img.mousePressEvent = self.MouseClickEvent
 
         else:
             self.statusbar.showMessage("No file has selected")
@@ -46,15 +48,35 @@ class MultiChannelWindow(QtWidgets.QMainWindow):
         file_name = QtWidgets.QFileDialog().getOpenFileName(self, "Select Ref Image", '',
                                                             'image file(*png *jpeg *tiff *tif )')
         if file_name[0]:
-            self.ref_image2 = cv2.imread(file_name[0])
-            self.ref_image2 = cv2.cvtColor(self.ref_image2, cv2.COLOR_BGR2RGB)
+            self.ref_image2 = tf.imread(file_name[0])
+            #self.ref_image2 = cv2.cvtColor(self.ref_image2, cv2.COLOR_BGR2RGB)
+
+            self.img2 = pg.ImageItem()
+            self.p1.addItem(self.img2)
+            colormap = cm.get_cmap("Greens")  # cm.get_cmap("CMRmap")
+            colormap._init()
+            lut = (colormap._lut * 255).view(np.ndarray)  # Convert matplotlib colormap from 0-1 to 0 -255 for Qt
+            self.img2.setImage(self.ref_image2,lut = lut)
+            self.img2.setCompositionMode(QtGui.QPainter.CompositionMode_Multiply)
+            #self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
+        else:
+            pass
+
+    def loadImage3(self):
+        file_name = QtWidgets.QFileDialog().getOpenFileName(self, "Select Ref Image", '',
+                                                            'image file(*png *jpeg *tiff *tif )')
+        if file_name[0]:
+            self.ref_image3 = tf.imread(file_name[0])
+            #self.ref_image2 = cv2.cvtColor(self.ref_image2, cv2.COLOR_BGR2RGB)
 
             self.img3 = pg.ImageItem()
             self.p1.addItem(self.img3)
-            self.img3.setImage(self.ref_image2)
-            self.img3.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
-            self.img.setZValue(10)
-
+            colormap = cm.get_cmap("Blues")  # cm.get_cmap("CMRmap")
+            colormap._init()
+            lut = (colormap._lut * 255).view(np.ndarray)  # Convert matplotlib colormap from 0-1 to 0 -255 for Qt
+            self.img3.setImage(self.ref_image3,lut = lut)
+            self.img3.setCompositionMode(QtGui.QPainter.CompositionMode_Multiply)
+            #self.img.setCompositionMode(QtGui.QPainter.CompositionMode_Plus)
         else:
             pass
 
